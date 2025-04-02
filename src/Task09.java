76ba5a0bf571fc0943fc4c30a498fab087e72185
Your Name <you@example.com>
2025-04-03 03:02:10 +0600

    task09

---
+++ src/Task09.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Task09 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: "); // 1001
+        String inputString = sc.nextLine();
+
+        boolean isBinary = true;
+
+        for (int i = 0; i < inputString.length(); i++) {
+            char ch = inputString.charAt(i);
+            if (ch != '0' && ch != '1') {
+                isBinary = false;
+                break;
+            }
+        }
+
+        System.out.println(isBinary); //true
+    }
+}
