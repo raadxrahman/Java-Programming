ccbb0c5615932998b3a6846ea15161c99994282c
Your Name <you@example.com>
2025-04-04 02:51:23 +0600

    updated gitignore

---
+++ src/Task11.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Task11 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string: "); //I am a SQA Engineer
+        String input = sc.nextLine();
+        String vowels = "AEIOUaeiou";
+        String result = "";
+
+        for (int i = 0; i < input.length(); i++) {
+            if (vowels.indexOf(input.charAt(i)) == -1) {
+                result += input.charAt(i);
+            }
+        }
+
+        System.out.println(result); // m sq ngnr
+    }
+}
